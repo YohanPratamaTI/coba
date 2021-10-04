a7ee94ad29ac23a129e845711f87b348dfe6081e
YohanesVito <yohanes.vito@ti.ukdw.ac.id>
2021-10-04 23:14:11 +0700

    rai gedhek ndastenk

---
+++ p5/app/src/main/java/com/example/pertemuan5/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.pertemuan5
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnLogin.setOnClickListener {
+            val user = inputUsername.text.toString()
+            val pass = inputPassword.text.toString()
+            if (pass == "1234") {
+                startActivity(Intent(this@MainActivity, Home::class.java))
+            } else {
+                Toast.makeText(applicationContext, "Username atau Password SALAH!!!", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+}
